4db67f7ce46a8d50fb486f31889cbea0a372352c
andralex8 <andralex8@gmail.com>
2015-12-31 14:30:43 +0100

    NFC fix
    Change-Id: I313dbe9deaea66bc212ad868a2e24fb659c45b56

--- msm8226.mk
+++ msm8226.mk
@@ -162,6 +162,8 @@ PRODUCT_PACKAGES += \
 ifeq ($(BOARD_HAS_NFC), true) 
 # NFC packages
 PRODUCT_PACKAGES += \
+    libnfc-nci \
+    libnfc_nci_jni \
     NfcNci \
     Tag \
     nfc_nci.pn54x.default \
@@ -176,6 +178,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
 
 NFCEE_ACCESS_PATH := $(LOCAL_PATH)/prebuilt/etc/nfcee_access.xml
 endif
